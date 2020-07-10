8bce732c2dbafcb9616c8aba0d8cdc78d4afc02b
Krzysztof Banasik <krzysztof.f.banasik@gmail.com>
2020-07-10 23:30:58 +0200

    Feature 02: Set up MinIO

--- src/main/java/krisbanas/awsdemo/AwsDemoApplication.java
+++ src/main/java/krisbanas/awsdemo/AwsDemoApplication.java
@@ -2,12 +2,19 @@ package krisbanas.awsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AwsDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDemoApplication.class, args);
+    }
 
+    @GetMapping
+    public String justATest() {
+        return "Hello there!";
+    }
 }
